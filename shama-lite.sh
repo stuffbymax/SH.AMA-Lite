591de4ca724e3e5e6c9f530b1939d3bf3ab875ad
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-05-19 02:09:42 +0100

    Update shama-lite.sh

--- shama-lite.sh
+++ shama-lite.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Hello ! 
-# This is a simple/minimal script 
-# To fetch some basic information about your system
-# I made this for my own personal use 
+# Hello!
+# This is a simple/minimal script to fetch some basic information about your system
+# I made this for my own personal use
 # It is limited as you can see
 # It's my own take on neofetch but less flashy and less fancy lol
-# I understand that my code isn't 5* quality ,so yeah ...
+# I understand that my code isn't 5* quality, so yeah ...
 # I only tested it on Kali and Ubuntu 
 # On other distros some commands won't work
 # So please feel free to change whatever command
-# Does not work with the appropriate one for your system .
+# Does not work with the appropriate one for your system.
 
-#Colors
+# Colors
 grey="\033[0;37m"
 purple="\033[0;35m"
 red="\033[1;31m"
@@ -20,61 +19,53 @@ green="\033[1;32m"
 yellow="\033[1;33m"
 transparent="\e[0m"
 
-#Commands & variables
+# Commands & Variables
 
-#OS 
-osTemp="$(cat /etc/os-release | grep "ID" | head -1)"
-readarray -d = -t strarr <<< $osTemp
-#removing trailing spaces
-os="$(echo -e "${strarr[1]}" | sed -e 's/[[:space:]]*$//')"
+# OS
+os="$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '\"')"
 
-#CPU info
-cpuTemp="$(cat /proc/cpuinfo | grep 'model name' |uniq)"
-readarray -d : -t strarrcpu <<< $cpuTemp
-cpu="$(echo -e "${strarrcpu[1]}"  | tr -d '[:space:]')"
+# CPU info
+cpu="$(awk -F: '/model name/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2; exit}' /proc/cpuinfo)"
 
-#RAM info
-ramTemp="$(cat /proc/meminfo | grep 'MemTotal')"
-readarray -d : -t strarrram <<< $ramTemp
-#trim white spaces and removes the kB at the end so it can be displayed as Mb afterwards
-ramx="$(echo -e "${strarrram[1]}"  | tr -d '[:space:]' | sed 's/..$//' )"
-ram=$((ramx / 1000))
+# RAM info
+ram_kb=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+ram=$((ram_kb / 1024)) # Convert from kB to MB
 
-#Hostname
-host="$(cat /proc/sys/kernel/hostname)"
+# Hostname
+host=$(cat /proc/sys/kernel/hostname)
 
-#Number of packages installed (everything included)
-pkgs="$(dpkg --get-selections | wc --lines)"
+# Number of packages installed (Debian-based)
+if command -v dpkg &> /dev/null; then
+  pkgs=$(dpkg --get-selections | wc -l)
+else
+  pkgs="N/A"
+fi
 
-#Uptime
-up="$(uptime | awk -F'( |,|:)+' '{d=h=m=0; if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0,"d,",h+0,"h,",m+0,"m."}')"
+# Uptime
+up=$(awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60; printf "%dd, %dh, %dm\n", d, h, m}' /proc/uptime)
 
+# GPU info
+gpu=$(lspci | grep VGA | cut -d ':' -f 3 | cut -d '[' -f 1 | sed 's/^ *//')
 
 # Main function
-Shama(){
-while true;
-do
-        #echo ""
-        echo -e "            "$green"——-"$purple"SH"$red"."$purple"AMA"$green"-——"   
-        echo -e ""    
-        echo -e "      "$green"|"$purple"■"$grey" OS     "  $red":" $grey" ${os^^}    "
-        echo -e "      "$purple"|"$green"■"$grey" UP     "  $red":" $grey" ${up}     "
-        echo -e "      "$green"|"$purple"■"$grey" CPU    "  $red":" $grey" ${cpu^^}     "
-        echo -e "      "$purple"|"$green"■"$grey" RAM    "  $red":" $grey" ${ram}Mb    "
-        echo -e "      "$green"|"$purple"■"$grey" HOST   "  $red":" $grey" ${host^^}     "
-        echo -e "      "$purple"|"$green"■"$grey" PKGS   "  $red":" $grey" ${pkgs}     "
-        echo ""
-
- break;
-done
-echo 
+display_info() {
+  echo -e "            ${green}——-${purple}SH${red}.${purple}AMA${green}-——"   
+  echo -e ""    
+  echo -e "      ${green}|${purple}■${grey} OS     ${red}: ${grey} ${os^^}"
+  echo -e "      ${purple}|${green}■${grey} UP     ${red}: ${grey} ${up}"
+  echo -e "      ${green}|${purple}■${grey} CPU    ${red}: ${grey} ${cpu^^}"
+  echo -e "      ${green}|${purple}■${grey} GPU    ${red}: ${grey} ${gpu}"
+  echo -e "      ${purple}|${green}■${grey} RAM    ${red}: ${grey} ${ram}MB"
+  echo -e "      ${green}|${purple}■${grey} HOST   ${red}: ${grey} ${host^^}"
+  echo -e "      ${purple}|${green}■${grey} PKGS   ${red}: ${grey} ${pkgs}"
+  echo -e ""
 }
 
-#Logo
+# Logo
 echo -e ""
 echo -e "               |\_/|"
-echo -e "               '"$yellow"o"$transparent"."$yellow"o"$transparent"'"  
+echo -e "               '"$yellow"o"$transparent"."$yellow"o"$transparent"'"
 echo -e "               > ^ <"
 
 # Main function init
-Shama;
+display_info
